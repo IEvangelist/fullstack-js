3ef4501dc4fff662db899cd427b3f49ec2c2100d
David Pine <david.pine@microsoft.com>
2025-04-02 12:04:30 -0500

    This should be good-to-go

--- src/Host/Program.cs
+++ src/Host/Program.cs
@@ -2,8 +2,6 @@ var builder = DistributedApplication.CreateBuilder(args);
 
 // Add our "api" weather forecast service
 
-
 // Add our "app" frontend and depend on the "api"
-
 
 builder.Build().Run();
